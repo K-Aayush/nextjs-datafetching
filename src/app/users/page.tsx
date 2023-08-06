0bdba193fec7f590c53f6dd87d6dd0b56b86a44a
Aayush <ayushkarkee6@gmail.com>
2023-08-06 15:23:56 +0545

    type of users

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import type { Metadata } from 'next'
+import GetAllUsers from '@/lib/getAllUsers'
+
+export const metadata: Metadata = {
+    title: 'Users',
+}
 
 const Users = () => {
+    const usersData: Promise<user[]> = GetAllUsers()
+
   return (
     <div>Users</div>
   )
