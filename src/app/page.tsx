5f41f4a98abcb1fddfd7063820da356161d26da8
Aayush <ayushkarkee6@gmail.com>
2023-08-06 15:43:22 +0545

    maping data on user page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className="">
       <div>Home</div>
+      <Link href="/users">Users</Link>
     </main>
   )
 }
--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,16 +1,47 @@
 import React from 'react'
 import type { Metadata } from 'next'
 import GetAllUsers from '@/lib/getAllUsers'
+import Link from 'next/link'
 
 export const metadata: Metadata = {
     title: 'Users',
 }
 
-const Users = () => {
-    const usersData: Promise<user[]> = GetAllUsers()
+const Users = async () => {
+    const usersData: Promise<user[]> = GetAllUsers();
+
+    const users = await usersData
+
+    const content = (
+        <section>
+            <h2>
+                <Link href="/">Back to Home</Link>
+            </h2>
+            <br />
+            {users.map((user) => (
+                <p key={user.id}>
+                    <Link href={`/users/${user.id}`}>{user.name}</Link>
+                </p>
+            ))}
+        </section>
+    )
+
 
   return (
-    <div>Users</div>
+    content
+
+    
+    // <div>
+    //     <h2>
+    //         <Link href="/">Back to Home</Link>
+    //     </h2>
+    //     <br />
+    //     {users.map((user) => (
+    //         <p key={user.id}>
+    //             <Link href={`/users/${user.id}`}>{user.name}</Link>
+    //         </p>
+    //     ))}
+    // </div>
   )
 }
 
